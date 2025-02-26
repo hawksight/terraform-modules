a7e90b1ae81c740f287cb3a911b885cc199514aa
Peter Fiddes <peter.fiddes@jestack.io>
2025-02-26 17:35:07 +0000

    feat: Example using terraform-provider-tlspc to onboard to TLSPC
    Signed-off-by: Peter Fiddes <peter.fiddes@jestack.io>

---
+++ examples/gke-tlspc-provider/inputs.tfvars.tpl
@@ -0,0 +1,13 @@
+# GCP Specific
+gcp_project = "REPLACE_ME"
+gcp_region = "europe-west1"
+gcp_zone = "europe-west1-c"
+gcp_cluster_name = "REPLACE_ME"
+
+# VCP Specific
+vcp_namespace="venafi"
+vcp_cluster_name="REPLACE_ME"
+vcp_team_owner="REPLACE_ME"
+vcp_team_name="REPLACE_ME"
+vcp_tenant_id="REPLAE_ME"
+vcp_region="eu"
---
+++ examples/gke-tlspc-provider/providers.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    tlspc = {
+      source = "jetstack/tlspc"
+      version = "0.1.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.14.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "6.12.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+  # credentials = file(pathexpand(var.gcp_svc_account_path))
+}
+
+provider "kubernetes" {
+  host                   = "https://${google_container_cluster.default.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(google_container_cluster.default.master_auth[0].cluster_ca_certificate)
+
+  ignore_annotations = [
+    "^autopilot\\.gke\\.io\\/.*",
+    "^cloud\\.google\\.com\\/.*"
+  ]
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.default.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.default.master_auth[0].cluster_ca_certificate)
+
+    # ignore_annotations = [
+    #   "^autopilot\\.gke\\.io\\/.*",
+    #   "^cloud\\.google\\.com\\/.*"
+    # ]
+  }
+}
+
+provider "tlspc" {
+  apikey   = trimspace(var.vcp_api_key)
+  endpoint = local.api_url
+}
